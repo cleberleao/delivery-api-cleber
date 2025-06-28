49ee0af8aebb56ebce40af6940eefba75d8d0110
cleberleao <cleberleao_14@hotmail.com>
2025-06-27 22:10:36 -0300

    inclusao cadastro produto

--- src/main/java/com/deliverytech/delivery_api/controller/ProdutoController.java
+++ src/main/java/com/deliverytech/delivery_api/controller/ProdutoController.java
@@ -1,7 +1,35 @@
 package com.deliverytech.delivery_api.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.deliverytech.delivery_api.entity.Produto;
+import com.deliverytech.delivery_api.services.ProdutoService;
+
+
 @RestController
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoController {
-    
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<?> cadastrar(@Validated @RequestBody Produto produto) {
+        try {
+            Produto produtoSalvo = produtoService.cadastrar(produto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(produtoSalvo);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Erro: " + e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro interno do servidor");
+        }
+    }
 }
--- src/main/java/com/deliverytech/delivery_api/services/ProdutoService.java
+++ src/main/java/com/deliverytech/delivery_api/services/ProdutoService.java
@@ -1,7 +1,18 @@
 package com.deliverytech.delivery_api.services;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.deliverytech.delivery_api.entity.Produto;
+import com.deliverytech.delivery_api.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
     
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto cadastrar(Produto produto) {
+
+        return produtoRepository.save(produto);
+    }
 }
